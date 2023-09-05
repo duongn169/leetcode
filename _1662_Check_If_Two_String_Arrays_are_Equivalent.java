848905316481f4f70d2d59cfd86a11f5b0a3b22d
duongn169 <103577284+duongn169@users.noreply.github.com>
2023-09-05 12:44:44 +0700

    String

--- _1662_Check_If_Two_String_Arrays_are_Equivalent.java
+++ _1662_Check_If_Two_String_Arrays_are_Equivalent.java
@@ -1,18 +1,28 @@
 public class _1662_Check_If_Two_String_Arrays_are_Equivalent {
     public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        String result1 ="";
-        String result2 = "";
+        // String result1 ="";
+        // String result2 = "";
 
+        // for (String string : word1) {
+        //     result1 += string;
+            
+        // }
+        // for (String string : word2) {
+        //     result2 += string;
+            
+        // }
+
+        // return result1.equals(result2);
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
         for (String string : word1) {
-            result1 += string;
-            
-        }
-        for (String string : word2) {
-            result2 += string;
-            
+            sb1.append(string);
         }
 
-        return result1.equals(result2);
+        for (String string : word2) {
+            sb2.append(string);
+        }
+        return sb1.toString().equals(sb2.toString());
     }
 
 
