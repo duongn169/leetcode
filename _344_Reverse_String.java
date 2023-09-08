7c0908667c455e813a23bdf29c37df9b4c08ed5d
duongn169 <103577284+duongn169@users.noreply.github.com>
2023-09-08 15:29:11 +0700

    binary search

--- _344_Reverse_String.java
+++ _344_Reverse_String.java
@@ -1,20 +1,32 @@
 public class _344_Reverse_String {
-    public static void swap(char[] arr, int i, int j) {
-        if(i < j) {
-            char t = arr[i];
-            arr[i] = arr[j];
-            arr[j] = t;
-            swap(arr, i + 1 , j - 1);
-        }
-    }
+    // public static void swap(char[] arr, int i, int j) {
+    //     if(i < j) {
+    //         char t = arr[i];
+    //         arr[i] = arr[j];
+    //         arr[j] = t;
+    //         swap(arr, i + 1 , j - 1);
+    //     }
+    // }
 
-    public static void reverseString(char[] s) {
-        swap(s, 0, s.length -1);
-    }
+    // public static void reverseString(char[] s) {
+    //     swap(s, 0, s.length -1);
+    // }
+     public static void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+        while (start < end) { 
+            char t = s[start];
+            s[start] = s[end];
+            s[end] = t;
+            start++;
+            end--;
+        }
+     }
 
 
     public static void main(String[] args) {
        char[] s = {'h','e','l','l','o'};
-       swap(s, 0, s.length -1);
+       reverseString(s);
+      
     }
 }
---
+++ _704_Binary_Search.java
@@ -0,0 +1,27 @@
+public class _704_Binary_Search {
+    public static int search(int[] a, int key) {
+
+        int n = a.length;
+        int iLeft = 0;
+        int iRight = n - 1;
+
+        while(iLeft <= iRight) {
+            int iMid = (iLeft + iRight) / 2;
+            System.out.println("["+ iLeft + "," +iRight+ "] xet " + iMid);
+            if(key == a[iMid]) {
+                return iMid;
+            }else if(key > a[iMid]) {
+                iLeft = iMid + 1;
+
+            }else {
+                iRight = iMid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {-1,0,3,5,9,12};
+        System.out.println(search(a, 2));
+    }
+}
